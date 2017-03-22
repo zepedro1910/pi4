0896567ddc2578bc0d7e1db2cba90c7208148238
Pedro <zpfsilva@gmail.com>
2017-03-22 16:28:07 -0300

    panels

--- WebContent/resources/js/controllers/cadastros-basicos.controller.js
+++ WebContent/resources/js/controllers/cadastros-basicos.controller.js
@@ -1,4 +1,17 @@
-cadastroModule.controller('cadastroController',['$scope','$http', function($scope, $http){
-	
-	
-}]);
+cadastroModule.controller('cadastroController', [
+      '$scope', '$http', 'PessoaService', function($scope, $http, service) {
+
+	      $scope.selectAll = function() {
+		      service.all().then(function(res) {
+			      $scope.pessoas = res.data;
+		      }, function(err) {
+			      console.log(err);
+		      });
+	      };
+
+	      $scope.save = function(pessoa) {
+		      service.save(pessoa);
+		      $scope.pessoa = {};
+	      };
+      }
+]);
--- src/br/com/senac/repository/PessoaRepositoryImpl.java
+++ src/br/com/senac/repository/PessoaRepositoryImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
@@ -18,6 +20,7 @@ public class PessoaRepositoryImpl implements PessoaRepository {
 
 	private final HibernateTemplate hibernateTemplate;
 	private final Validator validator;
+
 	@Inject
 	public PessoaRepositoryImpl(final HibernateTemplate hibernateTemplate, final Validator validator) {
 		super();
@@ -25,18 +28,25 @@ public class PessoaRepositoryImpl implements PessoaRepository {
 		this.validator = validator;
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
 	public List<Pessoa> allPessoas() {
-		return hibernateTemplate.loadAll(Pessoa.class);
+		final DetachedCriteria criteria = DetachedCriteria.forClass(Pessoa.class);
+		criteria.addOrder(Order.asc("id"));
+		// criteria.add(Restrictions.eq("nome", "pedro"));
+		return (List<Pessoa>) hibernateTemplate.findByCriteria(criteria);
+
 	}
 
+	@Override
 	@Transactional(propagation = Propagation.REQUIRED)
 	public Long savePessoa(final Pessoa pessoa) {
-		List<ConstraintViolation> validate = validator.validate(pessoa);
+		final List<ConstraintViolation> validate = validator.validate(pessoa);
 		System.out.println(validate.toString());
 		if (validate.size() == 0) {
 			final Pessoa mergePessoa = hibernateTemplate.merge(pessoa);
 			return mergePessoa.getId();
 		}
-	 return (long) 0;
+		return (long) 0;
 	}
 }
