b9273b6da0f3ac5d191535d21139c79eb128322f
JOSE PEDRO FRASSON DA SILVA <jose.pfsilva1@senacsp.edu.br>
2017-03-23 20:59:49 -0300

    clientes

--- WebContent/resources/js/controllers/cadastros-basicos.controller.js
+++ WebContent/resources/js/controllers/cadastros-basicos.controller.js
@@ -1,6 +1,15 @@
 cadastroModule.controller('cadastroController', [
       '$scope', '$http', 'PessoaService', function($scope, $http, service) {
-
+    	  
+    	  $scope.isNovo = false;
+    	  
+    	  service.all().then(function(res) {
+		      $scope.pessoas = res.data;
+	      }, function(err) {
+		      console.log(err);
+	      });
+    	  
+    	  
 	      $scope.selectAll = function() {
 		      service.all().then(function(res) {
 			      $scope.pessoas = res.data;
@@ -12,6 +21,13 @@ cadastroModule.controller('cadastroController', [
 	      $scope.save = function(pessoa) {
 		      service.save(pessoa);
 		      $scope.pessoa = {};
+		      
+		      $scope.isNovo = false;
 	      };
+	      
+	      $scope.novoCliente = function() {
+			$scope.isNovo = true;
+		}
+      
       }
 ]);
