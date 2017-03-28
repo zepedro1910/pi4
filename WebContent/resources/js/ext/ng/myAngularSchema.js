0701e93023634fa88ec69f2468d146dcb2a8d38d
Edward Silva <edward.silva@iteris.com.br>
2017-03-28 13:23:33 -0300

    adição da pagina 'index do e-commerce', com com js, css e imagens que compoem o designe do site

---
+++ WebContent/resources/js/ext/ng/myAngularSchema.js
@@ -0,0 +1,34 @@
+﻿var app = angular.module('app', []);
+app.controller('appController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+    $scope.produtos1 = [{
+        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner1.jpg',
+        titulo: 'Classico',
+        subtitulo: 'White',
+        preco: 'R$650,00'
+    }, {
+        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner2.jpg',
+        titulo: 'Classico',
+        subtitulo: 'White',
+        preco: 'R$550,00'
+    }, {
+        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner1.jpg',
+        titulo: 'Classico',
+        subtitulo: 'White',
+        preco: 'R$450,00'
+    }, {
+        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner2.jpg',
+        titulo: 'Classico',
+        subtitulo: 'White',
+        preco: 'R$350,00'
+    }, {
+        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner1.jpg',
+        titulo: 'Classico',
+        subtitulo: 'White',
+        preco: 'R$950,00'
+    }, {
+        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner2.jpg',
+        titulo: 'Classico',
+        subtitulo: 'White',
+        preco: 'R$850,00'
+    }];
+}]);
