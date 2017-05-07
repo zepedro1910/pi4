14918722ab1c7bc9d2dcc46bf9ce1715c6f6d6c5
Edward <edward.carvalho@gmail.com>
2017-05-07 14:00:55 -0300

    index-ecommerce buscando e mostrando produtos cadastrado no banco de dados

--- WebContent/resources/js/ext/ng/appAngular.js
+++ WebContent/resources/js/ext/ng/appAngular.js
@@ -2,38 +2,19 @@
 
 //controller ==> index-ecommerce.html
 app.controller('appController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
-
-    $scope.produtos1 = [{
-        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner1.jpg',
-        titulo: 'Classico',
-        subtitulo: 'White',
-        preco: 'R$650,00'
-    }, {
-        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner2.jpg',
-        titulo: 'Classico',
-        subtitulo: 'White',
-        preco: 'R$550,00'
-    }, {
-        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner1.jpg',
-        titulo: 'Classico',
-        subtitulo: 'White',
-        preco: 'R$450,00'
-    }, {
-        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner2.jpg',
-        titulo: 'Classico',
-        subtitulo: 'White',
-        preco: 'R$350,00'
-    }, {
-        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner1.jpg',
-        titulo: 'Classico',
-        subtitulo: 'White',
-        preco: 'R$950,00'
-    }, {
-        img: 'C:/Users/edward.silva/Documents/pi4/WebContent/resources/images/banner2.jpg',
-        titulo: 'Classico',
-        subtitulo: 'White',
-        preco: 'R$850,00'
-    }];
+	
+    $scope.produtos1 = [];
+    
+    $scope.loadIndexItems = function(){
+    	$http({
+    		method:'POST',
+    		url:'vinil/find',
+    		data: {maxResult:0, fistItem: 0}
+    	}).then(function(response){
+    		$scope.produtos1 = response.data;
+    	});
+    	
+    }
 }]);
 
 //controller ==> details-ecommerce.html
